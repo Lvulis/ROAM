b60e0e343520e9982b7006fb901938146cb18b97
Lawrence V <lawrence@vulis.net>
2021-06-15 15:07:35 -0700

    Tidied up code

--- OAM_functions.R
+++ OAM_functions.R
@@ -35,6 +35,7 @@ idIn <- cmpfun(function(idx, idy, nro, nco) {
          NA)
 })
 
+# 2D Rotation matrix
 cRotate <- cmpfun(function(theta) {
   matrix(c(cos(theta), sin(theta), -sin(theta), cos(theta)), nrow = 2, ncol = 2)
 })
@@ -43,9 +44,25 @@ deg2Rad <- cmpfun(function(deg) {
   deg*pi/180
 })
 
+
+computeAngle <- function(rot_array, d, hypo = sqrt(2/2)) {
+  # Use the approximation of seeAngles v2 to compute the viewing angle (note needs to include precision)
+  # Arguments:
+  #  rot_array: Array of rotation matrices (2x2x360/precision)
+  #  d: nx2 matrix of distances b/w testset & queryset
+  #  hypo: constant that's precomputed
+  # Returns:
+  #  sum of viewsheds which have unblocked view
+  oa <- apply(rot_array, 3,  function(ROT) {
+    spin <- ROT %*% t(d)
+    examine <- spin[, spin[1, ]>=0, drop = F]
+    (min(abs(examine[2, ]))  > hypo)*1L
+  })
+  sum(oa) 
+}
+
 openingAngle <- function(testset, queryset, precision = precision, NC = no_cores) {
-  # casts Rays and computes opening angle of the queryset against the test set
-  # Uses the approximation of seeangles2 from Shaw et al.
+  # Parallelization wrapper for computeAngles.
   # Arguments:
   #  testset: nx2 matrix containing x, y coordinates of test set pixel centers
   #  queryset: nx2 matrix containing x, y coordinates of query set pixelc enters
@@ -63,22 +80,17 @@ openingAngle <- function(testset, queryset, precision = precision, NC = no_cores
                               "theta", "hypo", "rot_array"),
                 envir=environment())
   # # Export objects from global environment
-  clusterExport(cl, varlist=c("deg2Rad","cRotate"))
+  clusterExport(cl, varlist=c("deg2Rad","cRotate", "computeAngle"))
   
   # rotate all of the differences by theta(j) around the center point
   
   oAset <- parSapply(cl, seq_len(r1), function(i) {
     d <- sweep(testset, 2,  queryset[i, ])
-    oa <- apply(rot_array, 3, function(ROT) {
-      spin <- ROT %*% t(d)
-      examine <- spin[, spin[1, ]>=0, drop = F]
-      (min(abs(examine[2, ]))  > hypo)*1L
-    })
-    sum(oa)
+    
+    computeAngle(rot_array, d, hypo)
+   
   }) * 360/precision
-  
   stopCluster(cl)
-  proc.time() - ptm
   return(oAset)
 }
 
@@ -86,7 +98,7 @@ openingAngle <- function(testset, queryset, precision = precision, NC = no_cores
 Seeangles2 <- function(watermask, precision = 360, no_cores = 3) {
   # Compute opening angle method with modifications for accuracy
   # Primary modification is an iterative expansion of the
-  # queryset. Requires pracma library 
+  # queryset. Requires pracma and EBImage libraries
   # Arguments:
   #  watermask: Binary watermask (0 = land, 1 = water)
   #  precision: Number of circular sectors to divide the viewshed of a pixel into
@@ -108,42 +120,40 @@ Seeangles2 <- function(watermask, precision = 360, no_cores = 3) {
   water_ind <- which(watermask>0.5)
   sea <- idExBase(water_ind, nrow(watermask), ncol(watermask))
   K   <- chull(allshore) #convex hull of the edge set
+  
+  # Test what points are *in* the convex hull. This is used to set up
+  # the maximum possible query set size
   In <- pracma::inpolygon(sea[, 1], sea[, 2], allshore[K, 1], allshore[K, 2]) # double check that row/column aren't fflippe
-  ## Sea outside the convex hull
-  ## Turn on if you want to show area outside CHULL but...not necessary
 
-  ## Sea within the convex hull originally from OAM code
-
+  # Preallocate a maximum query set size
   Shallowsea <- matrix(NA, nrow = sum(In), ncol = 3)
-  ## The "In" part is basically only to ensure you grow up to 
-  ## the convex hull, and isn't a bottleneck.
-  ######## INSERT DISPLAY CODE HERE
-
+ 
   ### Compute opening angle of the first set of pixels
   oA <- openingAngle(testset = allshore, queryset = Shallowsea_iter, precision = precision, NC = no_cores)
   
   ## We typically don't care about shorelines extracted at angles
   # greater than 90 degrees, so only compute angles up to that.
-  # This can be adjusted (not passed as an argument but could be)
+  # This can be adjusted (not passed as an argument but could be).
   notSL <- which(oA <= 90)
-  N_notSL <- length(notSL)
+  N_notSL <- length(notSL) # Check which values are less than a_max
   iter_bot <- nrow(Shallowsea_iter)
   
+  # 
   Shallowsea[1:nrow(Shallowsea_iter), 1:2] <- Shallowsea_iter
   Shallowsea[1:nrow(Shallowsea_iter), 3] <- oA
   
   gc()
-  bufferind <- which(watermask==0)
-  # while N of small viewshed objects is more than or equal to 1
-  # dilate the image around ONLY THOSE and look more
-  ptm <- proc.time()
+  bufferind <- which(watermask==0) # used for conditional dilation
+  
+  # while N of pixels w/ angle less than a_max is more than or equal to some max
+  # check the neighbors of those N pixels and compute their angles. Repeat until condition is met.
   while(N_notSL >= 100) {
     
     # construct empty matrix to grow
     tobuild <- matrix(0, nrow = nrow(watermask), ncol = ncol(watermask))
     tobuild[idIn(Shallowsea_iter[notSL, 1], Shallowsea_iter[notSL, 2], nrow(watermask), ncol(watermask))] <- 1L
     # grow the matrix
-    tobuild <- dilate(tobuild, makeBrush(3, 'box'))
+    tobuild <- EBImage::dilate(tobuild, makeBrush(3, 'box'))
     # drop any land pixels
     tobuild[bufferind] <- 0L
     # new possible pixels to get
@@ -166,19 +176,17 @@ Seeangles2 <- function(watermask, precision = 360, no_cores = 3) {
     gc()
     
   }
-  proc.time()- ptm
-  
+
   Shallowsea_short <- Shallowsea[!is.na(Shallowsea[, 1]), ]
   Shallowsea_ind <- idIn(Shallowsea_short[, 1], Shallowsea_short[, 2], nrow(watermask), ncol(watermask))
   
-  # Identify which pixels are in the shallowsea but haven't had OA computed
-  setabove <- water_ind[-match(Shallowsea_ind, water_ind)]
   # Matrix for final map that'll be output
   OA_map <- matrix(0, nrow = nrow(watermask), ncol = ncol(watermask))
   # Add in OA values for shallowsea pixels that were computed
   OA_map[Shallowsea_ind] <- Shallowsea_short[, 3]
-  # Remaining shallowsea pixels are 200
-  OA_map[setabove] <- 200L
+  
+  # Identify which pixels are in the shallowsea but haven't had OA computed,
+  OA_map[water_ind[-match(Shallowsea_ind, water_ind)]] <- 200L
   
   return(OA_map)
 }
