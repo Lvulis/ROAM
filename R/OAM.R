90acc9edc3aea4f23ba82a9f45a113ba0f867a6c
Lawrence V <lawrence@vulis.net>
2022-05-02 22:34:53 -0700

    Update parallel
    Updates the parallel argument.

--- R/OAM.R
+++ R/OAM.R
@@ -1,38 +1,34 @@
-#' Compute the Opening Angle Method
+#' Opening Angle Method
 #'
-#' test 1
+#' Compute the opening angle method
 #'
-#' This function computes the opening angle method based on Shaw et al., 2008 and through an a modification approximation used in Liang et al., (2015a). This approximation uses the rayshader package to efficiently cast rays from an angle phi to every pixel in the query set Q.
+#' @details This function computes the opening angle method based on Shaw et al., 2008 and through an a modification approximation used in Liang et al., (2015a). This R based version uses the rayshader package to efficiently cast rays from an angle phi to every pixel in the query set Q.
 #'
-#' @param watermask Numeric: binary matrix where 1 indicates water and 0 indicates land. Should be passed with georeferenced info.
+#' The parallel parameter takes integer values from 0 to 3.  0 will indicate no parallelization. Three parallelization schemes are possible: 1 use rayshaders-native parallelization; 2 use socket-based parallelization, neceessary on windows machines; 3 use fork-based parallelization, possible on *nix machines. Rayshaders native parallelization splits the input image into blocks and computes them in parallel, while the other 2 options will compute chunks of opening angles in parallel.
+#'
+#'
+#' @param watermask Numeric or stars: binary matrix where 1 indicates water and 0 indicates land. The georeferenced image as a stars object can be passed.
 #' @param precision Integer: Number of discrete angles to split circle into (angles tested given by 360/precision)
 #' @param save_im Logical: Whether or not the output should be written to a geoTIFF via stars
 #' @param fn_r Character: Output file name if save_im is true.
 #' @param ext st_crs: Geographic extent information from the stars object.
 #' @param no_cores Integer: Number of cores to use if running in parallel. Parallel is assumed by default.
-#' @param fork Logical: whether to use fork-type parallelization. See details. Only one of fork, socket, and native should be true.
-#' @param socket Logical: whether to use socket-type parallelization.
-#' @param native Logical: whether to use native-type parallelization.
-#'
+#' @param parallel Integer: Parallelization scheme (see details)
 #' @return Numeric: matrix with same dimensions as watermask
 #' @export
 #'
 #' @examples
 #' # See example in Github repo.
 OAM <- function(watermask, precision = 360,  save_im = T,
-                       fn_r = NULL, ext = NULL, no_cores = 3,
-                       fork = F, socket = F, native = F) {
-  # first get shoreline and pertinent border
-  if((save_im == T) & ((is.null(fn_r)) | (is.null(ext)))) stop("Provide write information")
-  partest = sum(c(fork, socket, native))
-  if(partest > 1) stop("Choose parallelization scheme")
-  if(partest <1) native <- T
-  # Arguments:
-  #  watermask: Binary watermask (0 = land, 1 = water)
-  #  precision: Number of circular sectors to divide the viewshed of a pixel into
-  #  no_cores: Number of cores
-  # Returns:
-  #  Opening Angle map (OA_map)
+                fn_r = NULL, ext = NULL, no_cores = 3,
+                parallel = 0) {
+  # Handle a stars-type input
+  if(methods::is(watermask, "stars")) {
+    ext <- stars::st_dimensions(watermask)$x$refsys
+    watermask <- matrix(as.matrix(watermask[[1]]), nrow = dim(watermask)[1], ncol = dim(watermask)[2])
+  }
+  if((save_im) & ((is.null(fn_r)) | (is.null(ext)))) stop("Provide write information")
+
   Slopes <- pracma::gradient(watermask)
   G <- sqrt(Slopes$X^2 + Slopes$Y^2)
   rm(Slopes)
@@ -51,11 +47,11 @@ OAM <- function(watermask, precision = 360,  save_im = T,
   # Test what points are *in* the convex hull. This is used to set up
   # the maximum possible query set size
   In <- pracma::inpolygon(sea[, 1], sea[, 2], allshore[K, 1], allshore[K, 2]) # double check that row/column aren't fflippe
-
   tm = sweepimage = matrix(0, nrow = nrow(edges), ncol = ncol(edges))
   tm[water_ind[In]] <- 1
 
-  if(native==T) {
+  if(parallel <= 1) {
+    if(parallel == 0) {no_cores = 1}
     print("Using rayshader's image breakdown parallelization")
     for(k in seq(360/precision, 360, length.out = precision)) {
       print(k)
@@ -65,13 +61,11 @@ OAM <- function(watermask, precision = 360,  save_im = T,
       gc()
     }
     sweepimage2 <- sweepimage[nrow(sweepimage):1, ]*360/precision
-
   }
 
   nb = precision/no_cores
 
-
-  if(fork) {
+  if(parallel == 3) {
     print("Parallelizing angles into chunks using fork (optimal on *nix)")
     sweepimage_par =  parallel::mclapply(0:(no_cores-1), \(j) {
       print(j)
@@ -85,7 +79,7 @@ OAM <- function(watermask, precision = 360,  save_im = T,
       return(sweepimage)
     })
   }
-  if(socket) {
+  if(parallel == 2) {
     print("Parallelizing angles into chunks using socket")
     cl <- parallel::makeCluster(no_cores)
 
@@ -111,7 +105,7 @@ OAM <- function(watermask, precision = 360,  save_im = T,
 
   }
 
-  if(socket | fork) {
+  if(parallel >= 2) {
     sweepimage2 = matrix(0, nrow = nrow(sweepimage), ncol = ncol(sweepimage))
     for (j in 1:(no_cores)) {
       sweepimage2 = sweepimage2 + sweepimage_par[[j]]
@@ -120,13 +114,9 @@ OAM <- function(watermask, precision = 360,  save_im = T,
 
   }
 
-
-
   sweepimage2[watermask==0] <- 0
 
   sweepimage2[water_ind[-match(water_ind[In], water_ind)]] <- 200L
-
-
 
   if(save_im == T) {
     OA_r <- stars::st_as_stars(sweepimage2)
@@ -136,7 +126,6 @@ OAM <- function(watermask, precision = 360,  save_im = T,
                        options = c("COMPRESS=LZW", "TFW=NO"))
 
   }
-
 
   return(sweepimage2)
 }
